c9dc95cc405dbe309c4c213e93595e31a632cb7b
Bikram Pun <bikrampun47@gmail.com>
2022-05-26 20:28:51 +0545

    Console folder is created and some file edited

--- console/src/main/java/org/example/console/Main.java
+++ console/src/main/java/org/example/console/Main.java
@@ -1,26 +1,28 @@
-package org.example;
+package org.example.console;
 
 
+import org.example.AppConfig;
+import org.example.MessageGenerator;
+import org.example.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static final Logger log= LoggerFactory.getLogger(Main.class);
     //now we are using annotation so no need of beans.xml
-    private static final String CONFIG_LOCATION = "beans.xml";
+
     public static void main(String[] args) {
         log.info("Guess the number game");
 
         //create context (container)
         ConfigurableApplicationContext context
-                =new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+                =new AnnotationConfigApplicationContext(AppConfig.class);
 
         //get numberGenerator bean from context (container)
         NumberGenerator numberGenerator
-                =context.getBean("numberGenerator",NumberGenerator.class);
+                =context.getBean(NumberGenerator.class);
 
         //call method next() to get a random number
         int number = numberGenerator.next();
@@ -29,12 +31,10 @@ public class Main {
         log.info("number = {}",number); //{} are just placeholder
         //here curly braces replace with argument specified after comma
 
-        //get game bean from context (container)
-        Game game = context.getBean(Game.class);
-
-        //call reset method
-        game.reset();
-
+        //get message generator bean from context (container)
+        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
+        log.info("getMainMessage = {}",messageGenerator.getMainMessage());
+        log.info("getResultMessage = {}",messageGenerator.getResultMessage());
 
         //close context (container)
         context.close();
